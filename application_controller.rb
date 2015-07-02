ebc572f66990078dbf4d7a2412d5b0c0e6c415fa
colinmsato <colinmsato@gmail.com>
2015-07-02 17:06:26 +0000

    add some logic to the post to profile method

--- application_controller.rb
+++ application_controller.rb
@@ -8,7 +8,14 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/profile' do
+    zipcode=params[:zipcode]
+    trees_with_zip_match=[]
+    trees.each |treenum,treeinfo| do
+      trees_with_zip_match.push({:treenum, treeinfo}) if treeinfo[:zip]=zipcode
+    end
     
+    
+    end
     
     erb :profile
   end
